d23c49ea6101dfa7cd2869e4e7c73d38440958cc
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-12 10:47:00 +0300

    added bit of html, js, and css

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 /*-------------- Constants -------------*/
 // 5. Game Rules and Setup: Define board size (8x8), colors for each player (black and white), and normal vs. king pieces.
-
+const boardSize = 8
 /*---------- Variables (state) ---------*/
 // 1. Variables Needed: Set up variables to track the board layout, which player’s turn it is, and possible moves.
-
+let currentPlayer = 'White'
 /*----- Cached Element References  -----*/
 // 2. Main Elements on Page: Link to the main game area, turn display, messages, and restart button.
+const boardEle = document.getElementById('board')
 
+const restartEle = document.getElementById('restart')
 /*-------------- Functions -------------*/
 // 3. Set Up the Game at Start: Load the board, pieces, and show whose turn it is.
+const createBoard = () => {
+  for (let i = 0; i < boardSize * boardSize; i++) {
+    const square = document.createElement('div')
+    square.classList.add('sqr')
+    square.id = i
+    boardEle.appendChild(square)
+  }
+}
 
 // 4. How the Game Looks: Show the pieces in their places and display whose turn it is.
 
@@ -28,3 +38,4 @@
 
 /*----------- Event Listeners ----------*/
 // 13. Add event listeners for piece clicks, square clicks, and the reset button
+document.addEventListener('DOMContentLoaded', createBoard)
